81de09d3d3b3955df7cbdb569298843d9c28d652
LoneVertex <minaalaa141@gmail.com>
2026-08-16 14:27:41 +0300

    chore(version): bump extension to 1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
     defaultConfig {
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
         applicationId = "eu.kanade.tachiyomi.extension.ar.procomic"
 
